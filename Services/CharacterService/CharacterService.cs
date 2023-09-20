e1cc87060f56828a6367267b11ff57938bace44c
Vitcovschi Andrei <vitcovschi86@gmail.com>
2023-09-20 13:14:07 +0300

    Used entity to make calls to the DB

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -1,5 +1,7 @@
 using System.ComponentModel;
 using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Tutorial_proj.Data;
 using Tutorial_proj.Models;
 
 namespace Tutorial_proj.Services.CharacterService
@@ -18,10 +20,32 @@ namespace Tutorial_proj.Services.CharacterService
         #region Constructor and Dependencies
 
         private readonly IMapper _mapper;
+        private readonly DataContext _context;
         
-        public CharacterService(IMapper mapper)
+        public CharacterService(IMapper mapper, DataContext context)
         {
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        #endregion
+
+        #region GET Methods 
+
+         public async Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters()
+        {
+            var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
+            var dbCharacters = await _context.Characters.ToListAsync();
+            serviceResponse.Data = dbCharacters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
+            return serviceResponse;
+        }
+
+        public async Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id)
+        {
+            var serviceResponse = new ServiceResponse<GetCharacterDto>();
+            var dbCharacter = await _context.Characters.FirstOrDefaultAsync(c => c.Id == id);
+            serviceResponse.Data = _mapper.Map<GetCharacterDto>(dbCharacter);
+            return serviceResponse;
         }
 
         #endregion
@@ -32,35 +56,14 @@ namespace Tutorial_proj.Services.CharacterService
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
             var character = _mapper.Map<Character>(newCharacter);
-            character.Id = characters.Max(c => c.Id) + 1;
-            characters.Add(character);
-            serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
-            return serviceResponse;
-        }
-
-        public async Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters()
-        {
-            var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
-            serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
-            return serviceResponse;
-        }
-
-        public async Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id)
-        {
-            var serviceResponse = new ServiceResponse<GetCharacterDto>();
-            var character = characters.FirstOrDefault(c => c.Id == id);
-
-            serviceResponse.Data = _mapper.Map<GetCharacterDto>(character);
-        #region Methods
-
-        public async Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter)
-        {
-            var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
-            var character = _mapper.Map<Character>(newCharacter);
             
+            _context.Characters.Add(character);
+            await _context.SaveChangesAsync();
+            serviceResponse.Data = await _context.Characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToListAsync();
             return serviceResponse;
         }
 
+       
         public async Task<ServiceResponse<GetCharacterDto>> UpdateCharacter(UpdateCharacterDto updatedharacter)
         {
             var serviceResponse = new ServiceResponse<GetCharacterDto>();
