edb1c8916238cfde0e3482f966816b50cf1f9906
Vitcovschi Andrei <vitcovschi86@gmail.com>
2023-09-20 11:32:02 +0300

    Added Migration

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
 global using Tutorial_proj.Dtos.Character;
+using Microsoft.EntityFrameworkCore;
+using Tutorial_proj.Data;
 using Tutorial_proj.Services.CharacterService;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<DataContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
